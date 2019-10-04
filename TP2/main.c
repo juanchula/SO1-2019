96e22869998f3417adb568016ea38b8cba34b23b
juanchula <jruloignacio@hotmail.com>
2019-10-04 09:52:29 -0300

    Cambiamos el pwd para probar

--- TP2/main.c
+++ TP2/main.c
@@ -2,12 +2,18 @@
 #include <stdio.h>
 #include <pwd.h>
 #include <unistd.h>
+#include <string.h>
 
+// void cd{
+
+// }
 int PrintPrompt(){
 
 	 uid_t id = geteuid();
 	 struct passwd *pwd = getpwuid(id);
-
+	 char* direc = pwd->pw_dir;
+	 pwd->pw_dir = strcat(direc, "/git");
+	 puts(pwd->pw_dir);
 
 	 FILE *machineName = fopen ("/proc/sys/kernel/hostname", "rb");
 	 char arg[200];
@@ -19,7 +25,11 @@ int PrintPrompt(){
 	return 0;
 
 }
-
+// void directory(char *url){
+//     if(-d url){
+        
+//     }
+// }
 int main(int argc, char **argv){
 	PrintPrompt();
 }
